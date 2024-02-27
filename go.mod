2cd2e1fcce9164d37628c986c15c88dd12fc1027
Vinu John <vinu_john@hotmail.com>
2024-02-26 16:18:01 -0800

    move reverse string

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/vinujohn/golang-learning
+
+go 1.21.5
---
+++ reverse_string_test.go
@@ -0,0 +1,28 @@
+package learning
+
+import "testing"
+
+// ReverseString returns its argument string reversed rune-wise left to right.
+func ReverseString(s string) string {
+	r := []rune(s)
+	for i, j := 0, len(r)-1; i < len(r)/2; i, j = i+1, j-1 {
+		r[i], r[j] = r[j], r[i]
+	}
+	return string(r)
+}
+
+func TestReverseString(t *testing.T) {
+	cases := []struct {
+		in, want string
+	}{
+		{"Hello, world", "dlrow ,olleH"},
+		{"Hello, 世界", "界世 ,olleH"},
+		{"", ""},
+	}
+	for _, c := range cases {
+		got := ReverseString(c.in)
+		if got != c.want {
+			t.Errorf("Reverse(%q) == %q, want %q", c.in, got, c.want)
+		}
+	}
+}
