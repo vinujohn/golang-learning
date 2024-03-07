c755826cd5d0acee79ba3eec28986ee1b57851b9
Vinu John <vinu_john@hotmail.com>
2024-03-06 17:45:13 -0800

    better algo

--- algo_max_profit_test.go
+++ algo_max_profit_test.go
@@ -7,17 +7,30 @@ import (
 )
 
 // sliding window problem
-// keep track of the min so as we go forward
+/*
+- We initialize `minPrice` to the first price in the array and `maxProfit` to 0.
+- We then iterate over the prices array. For each price:
+    - If the current price is less than `minPrice`, we update `minPrice` to the current price. This is because we've found a new minimum price that could potentially lead to a larger profit.
+    - If the difference between the current price and `minPrice` is greater than `maxProfit`, we update `maxProfit` to this difference. This is because we've found a new maximum profit.
+- At the end of the iteration, `maxProfit` will contain the maximum profit we could have made by buying at a certain price and then selling at a later price.
+*/
 func maxProfit(prices []int) int {
-	profit, min := 0, prices[0]
-	for i := 1; i < len(prices); i++ {
-		if prices[i] < min {
-			min = prices[i]
-		} else if (prices[i] - min) > profit {
-			profit = (prices[i] - min)
+	if len(prices) == 0 {
+		return 0
+	}
+
+	minPrice := prices[0]
+	maxProfit := 0
+
+	for _, price := range prices {
+		if price < minPrice {
+			minPrice = price
+		} else if price-minPrice > maxProfit {
+			maxProfit = price - minPrice
 		}
 	}
-	return profit
+
+	return maxProfit
 }
 
 func TestMaxProfit(t *testing.T) {
